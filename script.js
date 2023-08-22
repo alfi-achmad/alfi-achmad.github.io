e222e193c67ee3f48584caef58bbdca3b1fc9325
alfi-achmad <132832172+alfi-achmad@users.noreply.github.com>
2023-08-22 21:41:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//need jQuery??
+
+function exHello() {
+    alert('Hello, world!');
+}
+
+function isi_biography() {
+    var bio = isi;
+    for (x = 0; x < 3; x++) bio += bio;
+    return bio;
+}
+
+var isi = "Diajukan sebagai syarat lulus -Kelas Belajar Dasar Pemrograman Web-. ";
+
+
+
